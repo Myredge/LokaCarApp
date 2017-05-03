7bd523eadbb597f0a100cbaf53bb00b3f2eab41f
Myredge <gaetan.pen49@gmail.com>
2017-05-03 14:31:18 +0200

    Vue Détail Voiture

---
+++ app/src/main/java/BO/BO_Car.java
@@ -0,0 +1,47 @@
+package BO;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import Entities.Car;
+
+/**
+ * Created by gpensec2015 on 03/05/2017.
+ */
+
+public class BO_Car {
+
+    public void AddCar(){
+
+    }
+
+public void AddCar(Car car) {
+
+   /* JSONObject jsonObject = new JSONObject();
+    try {
+        jsonObject.put("strPlaque", car.getStrPlaque());
+        jsonObject.put("strMarque", car.getStrMarque());
+        jsonObject.put("strModele", car.getStrModele());
+        jsonObject.put("nbPlaces", car.getNbPlaces());
+        jsonObject.put("fPrixJour", car.getfPrixJour());
+        jsonObject.put("agence", car.getAgence());
+
+        //retrofit ou volley
+
+        HttpResponse response = Unirest.post("https://lokacar-c23b.restdb.io/rest/car")
+                .header("content-type", "application/json")
+                .header("x-apikey", "97d51d8956a3cb48a19f4edb1f1d1659a963d")
+                .header("cache-control", "no-cache")
+                .body(jsonObject.toString())
+                .asString();
+
+    } catch (JSONException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+
+    }
+    */
+}
+
+
+}
